64b4b9a9501da8de5780ae69cec9476281577f6d
simon <simon@stud.uni-due.de>
2014-12-13 21:25:29 +0100

    PUT to NCBI blast, first test

--- blast_web_testing.jl
+++ blast_web_testing.jl
@@ -1,11 +1,19 @@
 # Base URL: http://blast.ncbi.nlm.nih.gov/Blast.cgi?CMD=<command>{SPMamp;<name>=<value>}
 # Web API Documentation: http://www.ncbi.nlm.nih.gov/blast/Doc/urlapi.html
+#require("ArgParse")
+
 
 using HttpCommon
 using Requests
 using LightXML
+using FastaIO
 
+function main()
+  println(ARGS[1])
+  blast_put(ARGS[1])
+end
 base_url = "http://blast.ncbi.nlm.nih.gov/Blast.cgi"
+
 
 function blast_get_results(rid)
   url = "$( base_url )?CMD=Get&RID=$( rid )&FORMAT_TYPE=XML"
@@ -18,10 +26,12 @@ end
 
 function blast_put(query, database="nr", program="blastp", hitlist_size=500)
   query = encodeURI(query)
+  println(query)
   xml_doc = get("$( base_url )?CMD=Put&QUERY=$( query )&DATABASE=$( database )&program=$( program )&HITLIST_SZE=$( hitlist_size )")
   println(xml_doc.headers)
   #println(string(xml_doc))
 end
 
-blast_get_results("8NYCJ5XX014") 
+#blast_get_results("8NYCJ5XX014") 
 
+main()
