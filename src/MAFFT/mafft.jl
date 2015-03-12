1ec505146da9f20edb8d2e471312d0867fbc22d9
Henning Schumann <henning.schumann@gmail.com>
2015-03-12 15:03:02 +0100

    start mafft with input as string

--- src/MAFFT/mafft.jl
+++ src/MAFFT/mafft.jl
@@ -1,7 +1,7 @@
 # Julia Wrapper for MAFFT (http://mafft.cbrc.jp/alignment/software/)
 using FastaIO
 
-function mafft(fasta_in)
+function mafft(fasta_in::String)
 	fasta = readall(`mafft '--auto' '--quiet' $fasta_in`)
 	fr = readall(FastaReader(IOBuffer(fasta)))
 	return fr
@@ -9,3 +9,11 @@ end
 
 # Usage
 # mafft("examples/fasta/il4.fasta")
+
+function mafft_from_string(fasta_in::String)
+    # write to tempfile because mafft can not read from stdin
+    tempfile_path, tempfile_io = mktemp()
+    write(tempfile_io, fasta_in)
+    close(tempfile_io)
+	return mafft(tempfile_path)
+end
