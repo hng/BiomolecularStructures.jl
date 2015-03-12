2eb6985d38a9d198f7a7fa298458b5a7982a37ed
Henning Schumann <henning.schumann@gmail.com>
2015-03-12 14:34:09 +0100

    use IOBuffer(string) instead of workaround with tempfile

--- src/MAFFT/mafft.jl
+++ src/MAFFT/mafft.jl
@@ -3,19 +3,9 @@ using FastaIO
 
 function mafft(fasta_in)
 	fasta = readall(`mafft '--auto' '--quiet' $fasta_in`)
-	
-	#FastaIO doesn't do strings, we work around this with a temporary file
-	fasta_tmp = tempname()
-
-	f = open(fasta_tmp, "w")
-	write(f, fasta)
-
-	fr = readall(FastaReader(fasta_tmp))
-
-	rm(fasta_tmp)
-
+	fr = readall(FastaReader(IOBuffer(fasta)))
 	return fr
 end
 
 # Usage
-# mafft("examples/ex2")
+# mafft("examples/fasta/il4.fasta")
