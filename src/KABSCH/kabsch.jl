f94813011c3f70691811e00ea173c0deb5860991
simon <simon.malischewski@stud.uni-due.de>
2015-03-13 22:48:15 +0100

    use proper matrices, covariance matrix calculation

--- src/KABSCH/kabsch.jl
+++ src/KABSCH/kabsch.jl
@@ -1,22 +1,28 @@
 module Kabsch
 export calc_centroid, kabsch
-	# calculate a centroid of a 1-D array (vector)
-	function calc_centroid(centroid)
-		return map(x -> x/length(centroid),sum(centroid))
+	# calculate a centroid of a matrix
+	function calc_centroid(m)
+		sum_m = sum(m,1)
+		size_m = size(m)[2]
+
+		return map(x -> x/size_m, sum_m)
 	end
 
-	# Input: Two sets of Points: P, Q (Nx3 Matrices)
+	# Input: Two sets of Points: P, Q as Nx3 Matrices
+	# Zwei Proteinstrukturen P, Q mit je N Atomen
 	function kabsch(P,Q)
-		println(P)
+		
 		# Calculate centroids P, Q
+		# Die Massenzentren der Proteine
 		centroid_p = calc_centroid(P)
 		centroid_q = calc_centroid(Q)
 
 		# Translate P, Q so centroids are equal to the origin of the coordinate system
-		p_trans = map(x -> x - centroid_p, P)
-		q_trans = map(x -> x - centroid_q, Q)
+		# Translation der Massenzentren, so dass beide Zentren im Ursprung des Koordinatensystems liegen
+		P = broadcast(-,P, centroid_p)
+		Q = broadcast(-,Q, centroid_q)
 
-		
-		
+		# Compute covariance matrix
+		Cov = *(P', Q)		
 	end
 end
