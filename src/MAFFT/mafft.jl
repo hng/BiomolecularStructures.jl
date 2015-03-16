c7b9d7028252b4474a3f3aa5ae3954fb8348eb4a
Henning Schumann <henning.schumann@gmail.com>
2015-03-16 16:54:08 +0100

    MAFFT module

--- src/MAFFT/mafft.jl
+++ src/MAFFT/mafft.jl
@@ -1,16 +1,20 @@
 # Julia Wrapper for MAFFT (http://mafft.cbrc.jp/alignment/software/)
-using FastaIO
+module MAFFT
+export mafft, mafft_from_string
 
-function mafft(fasta_in::String)
-	fasta = readall(`mafft '--auto' '--quiet' $fasta_in`)
-	fr = readall(FastaReader(IOBuffer(fasta)))
-	return fr
-end
+    using FastaIO
 
-function mafft_from_string(fasta_in::String)
-    # write to tempfile because mafft can not read from stdin
-    tempfile_path, tempfile_io = mktemp()
-    write(tempfile_io, fasta_in)
-    close(tempfile_io)
-	return mafft(tempfile_path)
+    function mafft(fasta_in::String)
+        fasta = readall(`mafft '--auto' '--quiet' $fasta_in`)
+        fr = readall(FastaReader(IOBuffer(fasta)))
+        return fr
+    end
+
+    function mafft_from_string(fasta_in::String)
+        # write to tempfile because mafft can not read from stdin
+        tempfile_path, tempfile_io = mktemp()
+        write(tempfile_io, fasta_in)
+        close(tempfile_io)
+        return mafft(tempfile_path)
+    end
 end
