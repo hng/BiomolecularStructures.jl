f4b23a2b5ed240b368f5c1e399d23711ec0b6701
simon <simon.malischewski@stud.uni-due.de>
2015-03-17 01:03:22 +0100

    PDB -> Matrix of C_alpha atom coordinates

--- src/PDB/pdb.jl
+++ src/PDB/pdb.jl
@@ -6,10 +6,31 @@
 using PyCall
 @pyimport Bio.PDB as pdb
 
-pdbparser = pdb.PDBParser()
+# Read in a PDB file and return a matrix of C_Alpha atom coordinates
+function pdb_to_c_alpha_matrix(filename::String)
+	pdbparser = pdb.PDBParser()
 
-structure = pdbparser[:get_structure]("test", "test.pdb")
+	# parse the file
+	structure = pdbparser[:get_structure](filename, filename)
 
-for atom in structure[:get_atoms]()
-	println(atom[:get_coord]())
+	atoms = structure[:get_atoms]()
+	# Filter out C_Alpha atoms
+	atoms = filter(x -> x[:get_name]() == "CA", atoms) 
+	# Get the coordinates
+	atoms = map(x -> x[:get_coord](), atoms)
+
+	n = length(atoms)
+
+	# initialize an empty Nx3 matrix
+	matrix = zeros(n,3)
+
+	# fill it
+	for i in 1:n
+		matrix[i,:] = [atoms[i][1] atoms[i][2] atoms[i][3]]
+	end
+
+	matrix = reshape(matrix,n,3)
+
+	return matrix
 end
+
