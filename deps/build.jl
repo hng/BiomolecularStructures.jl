e7d334bb1498e6ab2a11dfbeaedf1c8f744ae67c
simon <simon.malischewski@stud.uni-due.de>
2015-03-24 23:15:15 +0100

    let's see if this breaks builds. probably.

--- deps/build.jl
+++ deps/build.jl
@@ -1,15 +1,2 @@
-using BinDeps
-@BinDeps.setup
-
-deps = [
-	mafft = library_dependency("mafft"),
-	biopython = library_dependency("python-biopython")
-]
-
-provides(AptGet,
-	{"mafft" => mafft})
-
-provides(AptGet,
-	{"python-biopython" => biopython})
-
-@BinDeps.install
+run(`sudo apt-get install mafft`)
+run(`sudo apt-get install python-biopython`)
