a6561aa71db4ca4fa9c2e1f741a1d20727fae6af
simon <simon.malischewski@stud.uni-due.de>
2015-03-24 14:45:41 +0100

    more intuitive check for reflection

--- src/KABSCH/kabsch.jl
+++ src/KABSCH/kabsch.jl
@@ -1,5 +1,5 @@
 module Kabsch
-export calc_centroid, kabsch, rotate, rmsd, translate_points, kabsch_rmsd, correct_reflection
+export calc_centroid, kabsch, rotate, rmsd, translate_points, kabsch_rmsd
 	# Calculate root mean square deviation of two matrices A, B
 	# http://en.wikipedia.org/wiki/Root-mean-square_deviation_of_atomic_positions
 	function rmsd(A::Array{Float64,2}, B::Array{Float64,2})
@@ -41,14 +41,6 @@ export calc_centroid, kabsch, rotate, rmsd, translate_points, kabsch_rmsd, corre
 		return P, Q, centroid_p, centroid_q
 	end
 
-	# corrects possible reflection
-	function correct_reflection(rotation::Array{Float64,2}, vt_z::Array{Float64,1})
-		if det(rotation) < 0.0
-			vt_z = -vt_z
-		end
-		return vt_z
-	end
-
 	# Input: Two sets of points: reference, coords as Nx3 Matrices (so)
 	# returns optimally rotated matrix 
 	function kabsch(reference::Array{Float64,2},coords::Array{Float64,2})
@@ -66,10 +58,12 @@ export calc_centroid, kabsch, rotate, rmsd, translate_points, kabsch_rmsd, corre
 		u::Array{Float64,2}, d::Array{Float64,1}, vt::Array{Float64,2} = svd(A)
 
 		# check for reflection
-		vt[:,3] = correct_reflection(*(vt, u'), vt[:,3])
-		
+		f::Int64 = sign(det(vt) * det(u))
+		m::Array{Int64,2} = [1 0 0; 0 1 0; 0 0 f]
+
 		# Calculate the optimal rotation matrix
-		rotation::Array{Float64,2} = *(vt, u')
+		rotation::Array{Float64,2} = *(m,*(vt, u'))
+
 		rotation = rotation'
 
 		# calculate the transformation
