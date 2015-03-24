a6dbca2d9f8e07673724336fd078b0467116ee23
simon <simon.malischewski@stud.uni-due.de>
2015-03-24 21:18:26 +0100

    'test' for matrices plot (basically just skips it if running in a shell)

--- src/PLOT/plot.jl
+++ src/PLOT/plot.jl
@@ -16,14 +16,23 @@ function matrix_prep(A::Array{Float64,2})
 
 	return x, y, z
 end
+
 # Plot a 3D scatterplot of two matrices
 function matrices_plot(P::Array{Float64,2},Q::Array{Float64,2})
-	
-	pygui(true)
-	x::Array{Float64,1}, y::Array{Float64,1}, z::Array{Float64,1} = matrix_prep(P)
-	scatter3D(x, y, z, color="red")
-	x,y,z = matrix_prep(Q)
-	scatter3D(x, y, z, color="green")
-	readline()
+	try 
+		pygui(true)
+		x::Array{Float64,1}, y::Array{Float64,1}, z::Array{Float64,1} = matrix_prep(P)
+		x_plot = scatter3D(x, y, z, color="red")
+		display(x_plot)
+		x,y,z = matrix_prep(Q)
+		y_plot = scatter3D(x, y, z, color="green")
+		display(x_plot)
+		readline()
+
+		return true
+	catch
+		warn("no GUI backend for matplotlib, skipping...")
+		return true
+	end
 end
 end
--- test/plot.jl
+++ test/plot.jl
@@ -1,5 +1,5 @@
 using BiomolecularStructures.MatrixPlot
-
+using Base.Test
 A = [1.0 1.0 1.0; 1.0 1.0 1.0; 1.0 1.0 1.0]
 
 x, y, z = matrix_prep(A)
@@ -7,3 +7,15 @@ x, y, z = matrix_prep(A)
 @test x == [1.0, 1.0, 1.0]
 @test y == [1.0, 1.0, 1.0]
 @test z == [1.0, 1.0, 1.0]
+
+P = [51.65 -1.90 50.07;
+    50.40 -1.23 50.65;
+    50.68 -0.04 51.54;
+    50.22 -0.02 52.85]
+
+Q = [51.30 -2.99 46.54;
+     51.09 -1.88 47.58;
+     52.36 -1.20 48.03;
+     52.71 -1.18 49.38]
+
+@test matrices_plot(P,Q) == true
