8f3a8d95e0f0b28257ef7feb64c57b14153b2711
simon <simon.malischewski@stud.uni-due.de>
2015-03-24 21:53:55 +0100

    PDB tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,4 @@ using Base.Test
 
 include(Pkg.dir("BiomolecularStructures", "test", "kabsch.jl"))
 include(Pkg.dir("BiomolecularStructures", "test", "plot.jl"))
+include(Pkg.dir("BiomolecularStructures", "test", "pdb.jl"))
