f49da16e9c06276bf26236f4fa3a3b5648d609ee
simon <simon.malischewski@stud.uni-due.de>
2015-03-26 16:29:05 +0100

    fastarepresentation test

--- test/hit.jl
+++ test/hit.jl
@@ -12,3 +12,7 @@ test_hit = Hit(1,"gi|18412314|ref|NP_565204.1|","tTest Hit","NP_565204",166,
 
 @test check_threshold(0.01, test_hit) == true
 @test check_threshold(2.9e-38, test_hit) == false
+
+# fastarepresentation test
+
+@test fastarepresentation(test_hit) == ("gi|18412314|ref|NP_565204.1|","MNQLQQLQNPGESPPVHPFVAPLSYLLGTWRGQGEGEYPTIPSFRYGEEIRFSHSGKPVIAY")
