a1950a796b3c54122a48c4c0569544cf781d335e
Henning Schumann <henning.schumann@gmail.com>
2015-03-26 18:06:55 +0100

    added Modeller and Mafft submodules

--- src/BiomolecularStructures.jl
+++ src/BiomolecularStructures.jl
@@ -5,4 +5,6 @@ module BiomolecularStructures
 	include(Pkg.dir("BiomolecularStructures", "src/PLOT/", "plot.jl"))	
 	include(Pkg.dir("BiomolecularStructures", "src/WebBLAST/", "hit.jl"))	
 	include(Pkg.dir("BiomolecularStructures", "src/WebBLAST/", "WebBLAST.jl"))	
+	include(Pkg.dir("BiomolecularStructures", "src/MODELLER/", "modeller.jl"))	
+	include(Pkg.dir("BiomolecularStructures", "src/MAFFT/", "mafft.jl"))	
 end
