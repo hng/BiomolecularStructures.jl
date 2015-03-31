d75b88eef23c87ab5bd344dcf633c90c4776f534
Henning Schumann <henning.schumann@gmail.com>
2015-03-31 13:36:28 +0200

    removed cli interface

--- src/WebBLAST/WebBLAST.jl
+++ src/WebBLAST/WebBLAST.jl
@@ -1,7 +1,5 @@
 module WebBLAST
-  require("ArgParse")
 
-  using ArgParse
   using FastaIO
   using HttpCommon
   using Requests
@@ -16,89 +14,8 @@ module WebBLAST
 
   export webblast
 
-  function parse_commandline()
-      s = ArgParseSettings(description = "WebBLAST")
-
-      @add_arg_table s begin
-          "--fasta", "-f"
-              nargs = 2
-              help = "Sequences in FASTA format, Sequence #"
-          "--sequence", "-s"
-              help = "Sequence as string"
-              arg_type = String
-          "--threshold", "-t"
-              help = "E-Value threshold"
-              arg_type = Float64
-              default = 0.0
-
-          "fasta_out"
-              help = "FASTA output file"
-              required = true
-              arg_type = String
-        end
     
-    return parse_args(s)
-  end
-
-  # reads a sequence from a FASTA file
-  function read_sequence(args) 
-    if length(args["fasta"]) >= 2
-
-      try
-        fasta = FastaReader(args["fasta"][1])
-        name = ""
-        seq = ""
-        try
-          for i in range(1,int(args["fasta"][2]))
-            name, seq = readentry(fasta)
-          end
-          
-        catch 
-          error("Error parsing arguments. Check 'em!")
-        end
-        return seq
-      catch GZError
-        error("File not found")
-      end
-
-    end
-
-    if args["sequence"] != ""
-      return args["sequence"]
-    end
-
-  end
-
-  # save results as fasta file
-  function save_as_fasta(results, filename)
-    data = {}
-    for result in results
-      push!(data, fastarepresentation(result))
-    end
-
-    writefasta(filename, data)
-  end
-
-  function main()
-    
-    args = parse_commandline()
-    seq = read_sequence(args)
-
-
-    threshold = args["threshold"]
-
-    info("Searching for sequence: $(seq)")
-
-    rid, rtoe = ncbi_blast_put(seq)
-    info("rid: $(rid)")
-    if ncbi_blast_search_info(rid)
-      results =  ncbi_blast_get_results(rid, threshold)
-      save_as_fasta(results, args["fasta_out"])
-    end
-  end
-
-  #main()
-
+  # searches webblast for fasta and returns results as Hit types
   function webblast(sequence::String, threshold::Float64=0.005, provider::Symbol=:ncbi, cached=false)
       info("Searching for sequence: $(sequence)")
 
@@ -132,5 +49,4 @@ module WebBLAST
         end
     end
   end
-
 end
