e4669cd70f1d83eafa3ccd020b01be4409793303
simon <simon.malischewski@stud.uni-due.de>
2016-02-24 01:11:49 +0100

    tests for mafft do not run reliably/reproducibly in v0.5

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,8 @@ include(Pkg.dir("BiomolecularStructures", "test", "kabsch.jl"))
 include(Pkg.dir("BiomolecularStructures", "test", "plot.jl"))
 include(Pkg.dir("BiomolecularStructures", "test", "pdb.jl"))
 include(Pkg.dir("BiomolecularStructures", "test", "hit.jl"))
-include(Pkg.dir("BiomolecularStructures", "test", "mafft.jl"))
+# mafft tests do not run reliably in v0.5
+if VERSION < v"0.5-"
+	include(Pkg.dir("BiomolecularStructures", "test", "mafft.jl"))
+end
 include(Pkg.dir("BiomolecularStructures", "test", "webblast.jl"))
